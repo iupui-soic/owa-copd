ee60b666a1f5e6dd1a2159f3157d71ba0abe74b5
Saptarshi Purkayastha <sunbiz@gmail.com>
2017-04-07 04:27:54 -0400

    display searched patients in table

--- app/js/cpgcopd.js
+++ app/js/cpgcopd.js
@@ -1,5 +1,7 @@
 function findPatients() {
     patientName = $("#searchpatient").val();
+    $("#hiddenPersons").text("");
+    $("#searchTableBody").html("");
     $.ajax({
         url: "https://gw151.iu.xsede.org/openmrs/ws/rest/v1/person?q=" + patientName,
         beforeSend: function (xhr) {
@@ -14,13 +16,41 @@ function findPatients() {
                     xhr.overrideMimeType("application/json; charset=x-user-defined");
                 }
             }).done(function (obsData) {
+                var persons = [];
                 for (j = 0; j < obsData.results.length; j++) {
                     visitText = obsData.results[j].display;
-                    if(visitText.includes('CHRONIC OBSTRUCTIVE PULMONARY DISEASE')){
-                        console.log(searchArray[i].uuid);
-                    };
+                    if (visitText.includes('CHRONIC OBSTRUCTIVE PULMONARY DISEASE')) {
+                        url = this.url;
+                        persons.push(url.substr(url.indexOf('=') + 1, url.length));
+                    }
+                    ;
+                }
+                persons = Array.from(new Set(persons));
+                for (i = 0; i < persons.length; i++) {
+                    $("#hiddenPersons").text($("#hiddenPersons").text() + ',' + persons[i]);
+                    $.ajax({
+                        url: "https://gw151.iu.xsede.org/openmrs/ws/rest/v1/person/" + persons[i],
+                        beforeSend: function (xhr) {
+                            xhr.overrideMimeType("application/json; charset=x-user-defined");
+                        }
+                    }).done(function (data) {
+                        $("#searchTableBody").html($("#searchTableBody").html() + '<tr onclick="loadObs(\''+data.uuid+'\')"><td>' + data.display + '</td><td>' + data.age + '</td><td>' + data.gender + '</td><td>' + data.uuid + '</td></tr>');
+                    });
                 }
             });
         }
     });
+}
+
+$(document).ready(function () {
+    var table = $('#searchTable').DataTable({
+        info: false,
+        searching: false,
+        paging: false,
+        ordering: false
+    });
+});
+
+function loadObs(uuid) {
+    console.log(uuid);
 }
